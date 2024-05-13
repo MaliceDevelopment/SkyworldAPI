867c32264b3a0d7f935d5f150637efbee4c6cac3
SamuelDeboni <samueldeboniss@gmail.com>
2024-05-13 17:35:39 -0300

    season

--- src/main/java/malicedev/skyworldapi/WorldTypeSkyblock.java
+++ src/main/java/malicedev/skyworldapi/WorldTypeSkyblock.java
@@ -4,20 +4,38 @@ import net.minecraft.core.world.World;
 import net.minecraft.core.world.biome.Biomes;
 import net.minecraft.core.world.biome.provider.BiomeProvider;
 import net.minecraft.core.world.biome.provider.BiomeProviderSingleBiome;
+import net.minecraft.core.world.config.season.SeasonConfig;
 import net.minecraft.core.world.generate.chunk.ChunkGenerator;
 import net.minecraft.core.world.generate.chunk.classic.ChunkGeneratorClassic;
 import net.minecraft.core.world.generate.chunk.empty.ChunkGeneratorEmpty;
+import net.minecraft.core.world.season.Seasons;
 import net.minecraft.core.world.type.WorldTypeEmpty;
+import net.minecraft.core.world.type.WorldTypeOverworldExtended;
+import net.minecraft.core.world.weather.Weather;
+import net.minecraft.core.world.wind.WindManagerGeneric;
 
 public class WorldTypeSkyblock extends WorldTypeEmpty {
+	public SeasonConfig seasonConfig;
 
 	public WorldTypeSkyblock(String languageKey) {
 		super(languageKey);
+
+		seasonConfig = SeasonConfig.builder()
+			.withSeasonInCycle(Seasons.OVERWORLD_SPRING, 14)
+			.withSeasonInCycle(Seasons.OVERWORLD_SUMMER, 14)
+			.withSeasonInCycle(Seasons.OVERWORLD_FALL, 14)
+			.withSeasonInCycle(Seasons.OVERWORLD_WINTER, 14)
+			.build();
 	}
 
 	@Override
 	public BiomeProvider createBiomeProvider(World world) {
 		return new BiomeProviderSingleBiome(Biomes.OVERWORLD_FOREST, 1.0, 1.0, 1.0);
+	}
+
+	@Override
+	public SeasonConfig getDefaultSeasonConfig() {
+		return seasonConfig;
 	}
 
 	@Override
